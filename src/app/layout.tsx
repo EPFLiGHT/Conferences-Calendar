c4e8e36cedd20b497a1478417bc0e1b37753e2cb
Azo <omarziyad.azgaoui2005@gmail.com>
2025-10-05 16:25:01 +0200

    Fix Timezone validation and Tag design (color schema)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" type="image/svg+xml" href={`${basePath}/light-logo.svg`} />
-        <link href="https://api.fontshare.com/v2/css?f[]=chillax@400,500,600,700&display=swap" rel="stylesheet" />
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=chillax@400,500,600,700&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <body>
+      <body style={{ margin: 0, padding: 0 }}>
         <ChakraProvider value={system}>
           {children}
         </ChakraProvider>
